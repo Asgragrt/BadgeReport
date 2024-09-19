06d35e0fbc56b0d32a43ecee89699660d89e0e7a
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-09-18 18:33:18 -0600

    🎨 Clean code

--- __init__.py
+++ __init__.py
@@ -1,41 +1,53 @@
-# import the main window object (mw) from aqt
-from aqt import mw
+"""
+Display remaining cards as app icon notification
+"""
 
-# import all of the Qt GUI library
-from aqt.qt import *
+from aqt import mw, gui_hooks
 
-from aqt import gui_hooks
-
-guiApplication = QGuiApplication.instance()
-
-
-cardCount = 0
+if mw is None:
+    raise TypeError("Main window is None!")
 
 
-def getRemainingCardCount() -> int:
-    try:
-        scheduler = mw.col.sched
-        count = sum(scheduler.counts())
-        return count
-    except:
-        return cardCount
+class Addon:
+    """Main addon class"""
+
+    def __init__(self) -> None:
+        self._card_count: int = 0
+
+    def get_card_count(self) -> int:
+        """Obtain card count"""
+        try:
+            if mw.col is None:
+                raise TypeError("Collection is None")
+
+            scheduler = mw.col.sched
+            count = sum(scheduler.counts())
+            return count
+        except:  # pylint: disable=bare-except
+            return self._card_count
+
+    def display_card_count(self) -> None:
+        """Display the card count"""
+        count = self.get_card_count()
+
+        # Clamp value
+        if count > 99:
+            count = 100
+
+        if count == self._card_count:
+            return
+
+        self._card_count = count
+        mw.app.setBadgeNumber(self._card_count)
 
 
-def displayCardCount(*_) -> None:
-    global cardCount
-
-    newCount = getRemainingCardCount()
-
-    # Clamp value
-    if newCount > 99:
-        newCount = 100
-
-    if newCount == cardCount:
-        return
-
-    cardCount = newCount
-    guiApplication.setBadgeNumber(cardCount)
+addon = Addon()
 
 
-gui_hooks.operation_did_execute.append(displayCardCount)
-gui_hooks.focus_did_change.append(displayCardCount)
+def display(*_) -> None:
+    """Display card count"""
+    addon.display_card_count()
+
+
+gui_hooks.operation_did_execute.append(display)  # type: ignore
+gui_hooks.focus_did_change.append(display)  # type: ignore
